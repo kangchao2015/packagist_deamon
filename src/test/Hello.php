2b6f1e32a42f80ab5f7ce8f8b952c88ed6d3788d
kangchao <kangchaolove2009@163.com>
2018-11-09 15:22:44 +0800

    a

---
+++ src/test/Hello.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namspace Kangchao/test;
+
+class Hello{
+  
+  public static function sayHello(){
+    echo "this is kangchao's first packagist", PHP_EOL;
+  }
+
+}
